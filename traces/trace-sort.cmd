fdf2769c4c32e2b596926beba81d436fb77eff02
yawen <zyw0919@gmail.com>
2024-03-02 21:45:00 +0800

    Import list_sort into the project

---
+++ traces/trace-sort.cmd
@@ -0,0 +1,9 @@
+# Test performance of sort betweem q_sort() and list_sort()
+# You can modify the option listsort and the RAND to meet your need
+
+option listsort 1
+new
+it RAND 500000
+time
+sort
+time
